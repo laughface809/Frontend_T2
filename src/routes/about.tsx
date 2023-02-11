38fc74ca2fb8616534d3e4ecf1bf49e4d20c0237
bsena-it <bsena@aidhub.org>
2023-02-12 00:05:20 +0800

    live facebook https://web.facebook.com/100001481286815/videos/866895534394746

---
+++ src/routes/about.tsx
@@ -0,0 +1,9 @@
+import { A } from "solid-start";
+
+export default function About() {
+  return (
+    <A href="/" class="text-sky-600 hover:underline">
+          Back to Home
+    </A>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+import Feature from "~/components/Feature";
+
+export default function Home() {
+  return (
+    <Feature />
+  );
+}
